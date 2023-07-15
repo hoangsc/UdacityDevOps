16ceb96670fc8b46cadc155afd216bc2b5c458e4
HoangChu <hoang.scv@gmail.com>
2023-07-15 19:10:41 +0000

    update

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ def predict():
         # Load pretrained model as clf. Try any one model. 
         #clf = joblib.load("./Housing_price_model/LinearRegression.joblib")
         #clf = joblib.load("./Housing_price_model/StochasticGradientDescent.joblib")
-        clf = joblib.load("./Housing_price_model/GradientBoostingRegressor.joblib")
-    except:
+        clf = joblib.load("StochasticGradientDescent.joblib")
+    except: # pylint: disable=bare-except
         LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
 
